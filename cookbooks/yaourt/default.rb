9bf136634fee56debf5a29ef4b997b88b2a5e956
upamune <jajkeqos@gmail.com>
2016-05-02 01:52:06 +0900

    add yaourtc

--- cookbooks/yaourt/default.rb
+++ cookbooks/yaourt/default.rb
@@ -7,6 +7,13 @@ remote_file "/etc/pacman.conf" do
   source "files/pacman.conf"
 end
 
+remote_file "/etc/yaourtrc" do
+  owner "root"
+  mode "0644"
+  group "users"
+  source "files/yaourtrc"
+end
+
 execute "update pacman.conf" do
   user "root"
   command "pacman --sync --noconfirm --refresh yaourt"
