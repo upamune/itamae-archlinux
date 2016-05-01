e2989e6b83321bc60e310870988d75e241c5a9a9
upamune <jajkeqos@gmail.com>
2016-05-01 23:14:06 +0900

    add cookbook for user

--- cookbooks/user/default.rb
+++ cookbooks/user/default.rb
@@ -1,12 +1,25 @@
 user_name='upamune'
 home_dir="/home/#{user_name}"
+pass_word='$6$salt$PNgsJo7ryMv5L7OeHCwQhtyKISMqCTdUMrlGfHl/xV2pUcVceMtpcXirzxfKzXwW9N3WKEtTuYEvFlWAoMJ6J0'
+wheel_gid='10'
 
 user 'create user' do
   action :create
   username user_name
+  password pass_word
   system_user true
   create_home true
   home home_dir
-  shell run_command("which zsh").stdout.strip
 end
 
+user user_name do
+  gid wheel_gid
+end
+
+# sudo可能にする
+remote_file "/etc/sudoers" do
+  source "files/sudoers"
+  mode   "440"
+  owner  "root"
+  group  "root"
+end
